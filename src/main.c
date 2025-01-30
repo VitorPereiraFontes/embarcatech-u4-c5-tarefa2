bffd59c8ab0421604f5d3c416fa0eacfee5081b0
VitorPereiraFontes <vitor.fontes002@gmail.com>
2025-01-30 18:50:37 -0300

    Adicionando a configuração do push button
    Definindo a GPIO a qual o botão está conectado.
    
    Modificando a constante 'LED_MASK' para 'GPIO_MASK' para que armazene a
    configuração de todas as GPIO's utilizadas no programa.
    
    Definindo a constante 'GPIO_VALUES_MASK' para configurar as GPIO's como
    entradas ou saídas.
    
    Adicionando um resistor de pull-up na GPIO do botão.

--- src/main.c
+++ src/main.c
@@ -2,13 +2,17 @@
 #include "pico/stdlib.h"
 #include "hardware/timer.h"
 
+// Define o pino do botão A
+#define A_BUTTON_PIN 5
+
 // Define quais pinos GPIO correspondem a cada cor do LED RGB 
 #define RED_LED_PIN 13
 #define GREEN_LED_PIN 11
 #define BLUE_LED_PIN 12
 
-// Define uma máscara de bits para a inicializaçao e configuração do LED RGB
-const uint32_t LEDS_GPIO_MASK = (1 << RED_LED_PIN) | (1 << GREEN_LED_PIN) | ( 1 << BLUE_LED_PIN);
+// Define uma máscara de bits para a inicializaçao e configuração dos valores das GPIO's
+const uint32_t GPIOS_MASK = (1 << RED_LED_PIN) | (1 << GREEN_LED_PIN) | ( 1 << BLUE_LED_PIN) | (1 << A_BUTTON_PIN);
+const uint32_t GPIOS_VALUES_MASK = (1 << RED_LED_PIN) | (1 << GREEN_LED_PIN) | ( 1 << BLUE_LED_PIN) | (0 << A_BUTTON_PIN); // 1 - Saída | 0 - Entrada
 
 int64_t alarm_callback(alarm_id_t id, void *user_data) {
     // Put your timeout handler code in here
@@ -18,9 +22,10 @@ int64_t alarm_callback(alarm_id_t id, void *user_data) {
 int main(){
     stdio_init_all(); // Inicializa a comunicação serial
 
-    // Inicializando as GPIO's do LED RGB como saídas e com o nível lógico baixo
-    gpio_init_mask(LEDS_GPIO_MASK); // Utiliza máscara de bits para inicializar os 3 pinos na mesma chamada de função
-    gpio_set_dir_masked(LEDS_GPIO_MASK,LEDS_GPIO_MASK); // Utiliza máscaras de bits para definir a direção de cada GPIO. Como todas serão saídas, apenas utilizei a mesma variável no segundo parâmetro.
+    // Inicializando as GPIO's do LED RGB como saídas e com o nível lógico baixo e a GPIO do botão A como entrada com um resistor de pull-up
+    gpio_init_mask(GPIOS_MASK); // Utiliza máscara de bits para inicializar as GPIO's
+    gpio_set_dir_masked(GPIOS_MASK,GPIOS_VALUES_MASK); // Utiliza máscaras de bits para definir a direção de cada GPIO.
+    gpio_pull_up(A_BUTTON_PIN); // Define que a GPIO do botão A terá um resistor de pull-up
 
     // Timer example code - This example fires off the callback after 2000ms
     add_alarm_in_ms(2000, alarm_callback, NULL, false);
