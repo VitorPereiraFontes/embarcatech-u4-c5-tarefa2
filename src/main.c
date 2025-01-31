f9cd4283bdfa471ca43b49cc36ca35e46411db6b
VitorPereiraFontes <vitor.fontes002@gmail.com>
2025-01-31 10:35:14 -0300

    Inicializando variáveis de controle
    Atualizando o nome das variáveis que armazenam as mascaras de bits.
    
    Criando uma máscara de bits apenas para as GPIO's do LED RGB.
    
    Alterando o nome da variável que habilita/desabilita o disparo do evento
    pelo botão.
    
    Definindo o tipo para uma estrutura de controle e inicializando-a, responsável por
    armazenar os dados entre as mudanças de estado do LED.

--- src/main.c
+++ src/main.c
@@ -11,38 +11,49 @@
 #define BLUE_LED_PIN 12
 
 // Define uma máscara de bits para a inicializaçao e configuração dos valores das GPIO's
-const uint32_t GPIOS_MASK = (1 << RED_LED_PIN) | (1 << GREEN_LED_PIN) | ( 1 << BLUE_LED_PIN) | (1 << A_BUTTON_PIN);
-const uint32_t GPIOS_VALUES_MASK = (1 << RED_LED_PIN) | (1 << GREEN_LED_PIN) | ( 1 << BLUE_LED_PIN) | (0 << A_BUTTON_PIN); // 1 - Saída | 0 - Entrada
+const uint32_t gpios_mask = (1 << RED_LED_PIN) | (1 << GREEN_LED_PIN) | ( 1 << BLUE_LED_PIN) | (1 << A_BUTTON_PIN);
+const uint32_t gpios_values_mask = (1 << RED_LED_PIN) | (1 << GREEN_LED_PIN) | ( 1 << BLUE_LED_PIN) | (0 << A_BUTTON_PIN); // 1 - Saída | 0 - Entrada
+uint32_t gpio_leds_mask = (1 << RED_LED_PIN) | (1 << GREEN_LED_PIN) | ( 1 << BLUE_LED_PIN); // Mascara de bits para os pinos GPIO do LED RGB
 
 // Inicializa uma variável de controle que armazena o estado do event de callback para o alarme
-bool event_triggered = false;
+bool event_running = false;
 
-int64_t alarm_callback(alarm_id_t id, void *user_data) {
-    // Put your timeout handler code in here
-    return 0;
-}
+// Define o intervalo de mudança entre os leds milissegundos
+uint led_change_interval = 3000; // 3 segundos
+
+// Define o intervalo de disparo do temporizador em milissegundos
+uint alarm_interval = 100;
+
+// Definindo uma estrutura de dados para o controle do evento
+typedef struct{
+    uint32_t led_gpios_values_mask; // Mascara de bits que informa qual cor do LED deverá ser habilitada
+    uint counter; // Contabiliza quantas mudanças o LED já sofreu
+}alarm_event_data_t;
+
+// Inicializando a estrutura com o estado inicial
+alarm_event_data_t initial_event_data = {
+    .led_gpios_values_mask = (1 << RED_LED_PIN) | (1 << GREEN_LED_PIN) | ( 1 << BLUE_LED_PIN), // Liga todos os LED's
+    .counter = 0
+};
 
 int main(){
     stdio_init_all(); // Inicializa a comunicação serial
 
     // Inicializando as GPIO's do LED RGB como saídas e com o nível lógico baixo e a GPIO do botão A como entrada com um resistor de pull-up
-    gpio_init_mask(GPIOS_MASK); // Utiliza máscara de bits para inicializar as GPIO's
-    gpio_set_dir_masked(GPIOS_MASK,GPIOS_VALUES_MASK); // Utiliza máscaras de bits para definir a direção de cada GPIO.
+    gpio_init_mask(gpios_mask); // Utiliza máscara de bits para inicializar as GPIO's
+    gpio_set_dir_masked(gpios_mask,gpios_values_mask); // Utiliza máscaras de bits para definir a direção de cada GPIO.
     gpio_pull_up(A_BUTTON_PIN); // Define que a GPIO do botão A terá um resistor de pull-up
-
-    // Timer example code - This example fires off the callback after 2000ms
-    add_alarm_in_ms(2000, alarm_callback, NULL, false);
-    // For more examples of timer use see https://github.com/raspberrypi/pico-examples/tree/master/timer
 
     while (true) {
         // Verifica se o botão foi pressionado e se o evento não foi disparado ainda
-        if (gpio_get(A_BUTTON_PIN) == 0 && !event_triggered){
+        if (gpio_get(A_BUTTON_PIN) == 0 && !event_running){
             sleep_ms(50); // Delay de 50 ms para prevenir debouncing do botão
 
             // Verifica se o botão ainda está pressionado
             if (gpio_get(A_BUTTON_PIN) == 0){
+                event_running = true ; // Altera o valor da variável de controle, inibindo o botão de ativar o evento
 
-                printf("Pressionado!\n");
+                printf("Pressionado!Agendando o temporizador para %d milissegundos no futuro...\n",alarm_interval);
             }
         }
 
